887b4e314b215da93199c460d0706cb3f549c232
Daniel Gustavo Aniceto <71411226+Danielpng-code@users.noreply.github.com>
2021-11-28 14:44:16 -0300

    Delete ex51 - PROGRESSAO ARITMETICA.py

--- ex51 - PROGRESSAO ARITMETICA.py
+++
@@ -1,8 +0,0 @@
-primeiro = int(input('Digite aqui o primeiro termo: '))
-razao = int(input('Digite aqui a razão: '))
-decimotermo = primeiro + (10) * razao
-for contador in range(primeiro, decimotermo, razao):
-    print('{}'.format(contador), end='- ')
-print('Acabamos!!!!')
-
-
